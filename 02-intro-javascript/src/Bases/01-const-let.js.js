f1345dfe8e96a87c31817a4cebfd630fec8d4e9c
yego0210 <diegotrujillo0210@gmail.com>
2021-09-26 00:27:40 -0500

    Curso de React
    Mis primeras clases de react

---
+++ 02-intro-javascript/src/Bases/01-const-let.js.js
@@ -0,0 +1,14 @@
+const nombre = 'Diego';
+const apellido = 'Trujillo';
+
+let valorDorado = 5
+valorDorado = 4
+
+console.log(nombre, apellido)
+
+
+if (true) {
+    const nombre = 'sara'
+    console.log(nombre)
+}
+console.log(valorDorado)
