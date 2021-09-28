0fbbd040ef9de39b4c09a2f7b4f3bafe0e2357ed
yego0210 <diegotrujillo0210@gmail.com>
2021-09-27 22:50:37 -0500

    Async  - - No terminado

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -22,5 +22,33 @@
 // @ -- Asynnc  Await  
 
 
-// ------   Asynnc  Await   CARPETA  3 VIDEO 15
+// ------   Async  Await   CARPETA  3 VIDEO 15
 //  ---------------------------------------------------------------------
+
+// const getImagenPromesa = () => new Promise(resolve => resolve('https://xfghdfgxdfgxdfsg.com'));
+// getImagenPromesa().then(console.log)
+
+const getImagen = async() => {
+
+}
+
+console.log(getImagen())
+
+
+
+// const apiKey = 'VS6J8dKQJhwl8oPgNpnosYucs9gKJV3t';
+
+// const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+
+// peticion
+//     .then(resp => resp.json()) 
+//     .then( ({ data }) => {
+//         const { url } = data.images.original;
+
+//         const img  = document.createElement('img')
+//         img.src = url 
+
+//         document.body.append( img )
+
+//     })
+//     .catch( console.warn());
