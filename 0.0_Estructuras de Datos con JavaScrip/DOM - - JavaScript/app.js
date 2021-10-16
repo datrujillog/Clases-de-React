a55145caefeecd8fb72d89ffd375b40956824bcc
yego0210 <diegotrujillo0210@gmail.com>
2021-10-16 00:04:03 -0500

    Diego - - React - - Primeros pasos

---
+++ 0.0_Estructuras de Datos con JavaScrip/DOM - - JavaScript/app.js
@@ -0,0 +1,74 @@
+// document.getElementById('nose').addEventListener("click", function(event) {
+//         iziToast.success({
+//             title: 'felicitaciones',
+//             position: 'topRight',
+//             color: 'red',
+//             message: 'tas has siscripto a nustro boletin',
+//         });
+//     })
+// swal({
+//     title: "Good job!",
+//     text: "You clicked the button!",
+//     icon: "success",
+// });
+//})
+
+// $(document).ready(function() {
+//     iziToast.warning({
+//         title: 'Caution',
+//         position: 'topRight',
+//         color: 'red',
+//         message: 'You forgot important data',
+//     });
+// })
+
+// function validar() {
+//     nombre = document.getElementById('inp').value;
+
+//     formato = /\w+@\w+\.+[a-z]/;
+//     if (nombre.length === 0 || apellido.length === 0 ||
+//         /^\s+$/.test(apellido, nombre, )) {
+
+//         iziToast.error({
+//             title: 'Error',
+//             position: 'topCenter',
+//             // color: 'red',
+//             message: 'los campos no deben deir vacios',
+//         });
+//         return false;
+//     } else {
+//         iziToast.success({
+//             title: 'registro ',
+//             position: 'topCenter',
+//             // color: 'red',
+//             message: 'Exitoso',
+//         });
+
+//     }
+// }
+
+
+
+
+// document.getElementById('nose').addEventListener("click", function(event) {
+//     validar()
+// })
+
+nombre = document.querySelector('#don');
+nombre.innerText += ' Diego Trujillo Galeano'
+nombre.style.color = 'red'
+nombre.style.background = 'green';
+nombre.style.height = '100px';
+let c = 0;
+nombre = document.getElementById('nose').addEventListener('click', function(e) {
+    si = document.getElementById('cual').innerHTML = `Total ${c++}`
+
+})
+
+
+function pero() {
+    console.log(c++)
+        // 
+}
+
+cont = sdfs() = {}
---
+++ 3-counter-app/src/PrimeraApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+
+//FC
+const PrimeraApp = () => { 
+    return <h1> Aprendiendo React</h1>;
+}
+
+
+
+export default PrimeraApp;
